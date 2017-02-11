5cbbaee3c875ce67844d8cdc89b9652ac4484ca5
Thecrazyskull <anaskarbila@gmail.com>
2017-02-11 18:45:26 -0500

    Launcher3: import reversed google now lib
    * Imported from https://github.com/patriksletmo/launcherclient/
    
    Change-Id: I84582ae812b8ecb0d694ae2396843effdcf1219c
    
    Launcher3: support google now tab
    
    Change-Id: I6efc1d5186e0f1bd4fa78ae7231999b984ce7222
    
    LauncherClient: make sure service is connected before trying to unbind
    
    Change-Id: If092db391be12db4f89ed292d1a2f5f9daef8c17
    
    Launcher3: animate workspace when animating the Google now page
    
    Change-Id: I6306d8eeee5ef87b3faf1d9dcb6825e67eb65d45
    
    Launcher3: make Google now left page optional
    
    * Always destroy the launcher client when disabled, don't just disable the scrolling page.
    
    Change-Id: Ic9a990c03c29e3ef280242c7a2d8b3bb0d049566
    
    Launcher3: cleanup launcher tab preference
    
     Remove preference is search package is not available
     Consolidate BroadcastReceiver and IntentFilter
    
    [OmniChange]
    
    Launcher3: Add OnStart and OnStop to LuncherClient
    
    Launcher3: Add ChangeListener for GoogleFeed
    
    Change-Id: Idcdf7380ce25a66f1dc6c3cd993ae76d089cdd36
    
    [On A11]
    Updated to use the new LauncherOverlay plugin
    
    [On A12]
    Replace isAppInstalled with isAvailableApp
    Remove previous Launchertab
    Remove previous Google LauncherClient.
    No more needed to edit the Launcher.java :-)
    
    Add prebuilt libGoogleFeed.jar for the new LauncherClient with:
     * ClientOptions
     * dump(String, PrintWriter)
     * hideOverlay(Int)
     * reattachOverlay
     * showOverlay(boolean)
    
    Imported from https://github.com/LineageOS/android_packages_apps_Trebuchet/commit/100eaee2247996010137dc5a1422e65c629d34f6
    Added the new OverlayCallbackImpl from LOS.
    
    Launcher3: move libGoogleFeed.jar to vendor/gapps
    
    and only GAPPS builds will add it
    for all other there is a mock impl
    
    Change-Id: I5382d538b28271388abea3809ca543814f720ec8

---
+++ src_overlay/mock/com/android/launcher3/overlay/OverlayCallbackImpl.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3.overlay;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import com.android.launcher3.Launcher;
+import com.android.systemui.plugins.shared.LauncherOverlayManager;
+import com.android.systemui.plugins.shared.LauncherOverlayManager.LauncherOverlay;
+import com.android.systemui.plugins.shared.LauncherOverlayManager.LauncherOverlayCallbacks;
+
+import com.google.android.libraries.gsa.launcherclient.LauncherClientCallbacks;
+
+import java.io.PrintWriter;
+
+/**
+ * Implements {@link LauncherOverlay} and passes all the corresponding events to {@link
+ * LauncherClient}. {@see setClient}
+ *
+ * <p>Implements {@link LauncherClientCallbacks} and sends all the corresponding callbacks to {@link
+ * Launcher}.
+ */
+public class OverlayCallbackImpl
+        implements LauncherOverlay, LauncherClientCallbacks, LauncherOverlayManager,
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+    public OverlayCallbackImpl(Launcher launcher) {
+    }
+
+    @Override
+    public void onDeviceProvideChanged() {
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+    }
+
+    @Override
+    public void dump(String prefix, PrintWriter w) {
+    }
+
+    @Override
+    public void openOverlay() {
+    }
+
+    @Override
+    public void hideOverlay(boolean animate) {
+    }
+
+    @Override
+    public void hideOverlay(int duration) {
+    }
+
+    @Override
+    public boolean startSearch(byte[] config, Bundle extras) {
+        return false;
+    }
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle bundle) {
+        // Not called
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle bundle) { }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+    }
+
+    @Override
+    public void onServiceStateChanged(boolean overlayAttached, boolean hotwordActive) {
+    }
+
+    @Override
+    public void onOverlayScrollChanged(float progress) {
+    }
+
+    @Override
+    public void onScrollInteractionBegin() {
+    }
+
+    @Override
+    public void onScrollInteractionEnd() {
+    }
+
+    @Override
+    public void onScrollChange(float progress, boolean rtl) {
+    }
+
+    @Override
+    public void setOverlayCallbacks(LauncherOverlayCallbacks callbacks) {
+    }
+}
---
+++ src_overlay/mock/com/google/android/libraries/gsa/launcherclient/LauncherClientCallbacks.java
@@ -0,0 +1,5 @@
+package com.google.android.libraries.gsa.launcherclient;
+public interface LauncherClientCallbacks {
+    void onOverlayScrollChanged(float progress);
+    void onServiceStateChanged(boolean overlayAttached, boolean hotwordActive);
+}
