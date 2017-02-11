b6775e6ff52724bc7760de307fda229cbdfe5cfb
Thecrazyskull <anaskarbila@gmail.com>
2017-02-11 18:45:26 -0500

    Launcher3: import reversed google now lib
    * Imported from https://github.com/patriksletmo/launcherclient/
    
    Change-Id: I84582ae812b8ecb0d694ae2396843effdcf1219c
    
    Launcher3: support google now tab
    
    Change-Id: I6efc1d5186e0f1bd4fa78ae7231999b984ce7222
    
    LauncherClient: make sure service is connected before trying to unbind
    
    Change-Id: If092db391be12db4f89ed292d1a2f5f9daef8c17
    
    Launcher3: animate workspace when animating the Google now page
    
    Change-Id: I6306d8eeee5ef87b3faf1d9dcb6825e67eb65d45
    
    Launcher3: make Google now left page optional
    
    * Always destroy the launcher client when disabled, don't just disable the scrolling page.
    
    Change-Id: Ic9a990c03c29e3ef280242c7a2d8b3bb0d049566
    
    Launcher3: cleanup launcher tab preference
    
     Remove preference is search package is not available
     Consolidate BroadcastReceiver and IntentFilter
    
    [OmniChange]
    
    Launcher3: Add OnStart and OnStop to LuncherClient
    
    Launcher3: Add ChangeListener for GoogleFeed
    
    Change-Id: Idcdf7380ce25a66f1dc6c3cd993ae76d089cdd36
    
    [On A11]
    Updated to use the new LauncherOverlay plugin
    
    [On A12]
    Replace isAppInstalled with isAvailableApp
    Remove previous Launchertab
    Remove previous Google LauncherClient.
    No more needed to edit the Launcher.java :-)
    
    Add prebuilt libGoogleFeed.jar for the new LauncherClient with:
     * ClientOptions
     * dump(String, PrintWriter)
     * hideOverlay(Int)
     * reattachOverlay
     * showOverlay(boolean)
    
    Imported from https://github.com/LineageOS/android_packages_apps_Trebuchet/commit/100eaee2247996010137dc5a1422e65c629d34f6
    Added the new OverlayCallbackImpl from LOS.
    
    Launcher3: move libGoogleFeed.jar to vendor/gapps
    
    and only GAPPS builds will add it
    for all other there is a mock impl
    
    Launcher3: move overlay lib to common repo
    
    Change-Id: Ief90e95a657aea1182f76c656eb8dfdafc3db505
    
    fixup! QuickStep: Google Feed integration
    
    Change-Id: Ic9ae500a93b930590e22eddb0fefe04deb584300
    
    Change-Id: Ief90e95a657aea1182f76c656eb8dfdafc3db505

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -63,6 +63,7 @@ import com.android.launcher3.Utilities;
 import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.SettingsCache;
 
+import org.omnirom.omnilib.utils.PackageUtils;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -94,6 +95,9 @@ public class SettingsActivity extends FragmentActivity
 
     private static final int DELAY_HIGHLIGHT_DURATION_MILLIS = 600;
     public static final String SAVE_HIGHLIGHTED_KEY = "android:preference_highlighted";
+
+    private static final String SEARCH_PACKAGE = "com.google.android.googlequicksearchbox";
+    private static final String SHOW_LEFT_TAB_PREFERENCE_KEY = "pref_left_tab";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -223,6 +227,17 @@ public class SettingsActivity extends FragmentActivity
 
             Preference showQsbWidget = findPreference(Utilities.QSB_SHOW);
             showQsbWidget.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    new Handler().postDelayed(() -> Utilities.restart(getActivity()), Utilities.WAIT_BEFORE_RESTART);
+                    return true;
+                }
+            });
+
+            Preference leftTabPage = findPreference(SHOW_LEFT_TAB_PREFERENCE_KEY);
+            if (!isSearchInstalled()) {
+                getPreferenceScreen().removePreference(leftTabPage);
+            }
+            leftTabPage.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
                 public boolean onPreferenceChange(Preference preference, Object newValue) {
                     new Handler().postDelayed(() -> Utilities.restart(getActivity()), Utilities.WAIT_BEFORE_RESTART);
                     return true;
@@ -424,6 +439,10 @@ public class SettingsActivity extends FragmentActivity
                 return Collections.emptyList();
             }
             return result;
+        }
+
+        private boolean isSearchInstalled() {
+            return PackageUtils.isAvailableApp(SEARCH_PACKAGE, getActivity());
         }
     }
 }
