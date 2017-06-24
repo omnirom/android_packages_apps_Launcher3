01a0957335f83f6aabafca5f08cb7ed12c879ced
maxwen <max.weninger@gmail.com>
2017-06-24 02:07:03 +0200

    Launcher3: full icon pack support
    Change-Id: I879f1eb6294d2670c2fd100777f48c4a4a9a8122

--- src/com/android/launcher3/IconPackProvider.java
+++ src/com/android/launcher3/IconPackProvider.java
@@ -10,11 +10,17 @@ import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class IconPackProvider {
     private static Map<String, IconPack> iconPacks = new ArrayMap<>();
+    static final String ICON_MASK_TAG = "iconmask";
+    static final String ICON_BACK_TAG = "iconback";
+    static final String ICON_UPON_TAG = "iconupon";
+    static final String ICON_SCALE_TAG = "scale";
 
     public static IconPack getIconPack(String packageName){
         return iconPacks.get(packageName);
@@ -49,6 +55,10 @@ public class IconPackProvider {
 
     private static Map<String, String> parseAppFilter(XmlPullParser parser) throws Exception {
         Map<String, String> entries = new ArrayMap<>();
+                
+        Map<String, String> iconPackResources = new HashMap<String, String>();
+        List<String> iconBackStrings = new ArrayList<String>();
+        
         while (parser.next() != XmlPullParser.END_DOCUMENT) {
             if (parser.getEventType() != XmlPullParser.START_TAG) {
                 continue;
@@ -60,6 +70,40 @@ public class IconPackProvider {
                 if (drawable != null && comp != null) {
                     entries.put(comp, drawable);
                 }
+                continue;
+            }
+
+            if (name.equalsIgnoreCase(ICON_BACK_TAG)) {
+                String icon = parser.getAttributeValue(null, "img");
+                if (icon == null) {
+                    for (int i = 0; i < parser.getAttributeCount(); i++) {
+                        iconBackStrings.add(parser.getAttributeValue(i));
+                    }
+                }
+                continue;
+            }
+
+            if (name.equalsIgnoreCase(ICON_MASK_TAG) ||
+                    name.equalsIgnoreCase(ICON_UPON_TAG)) {
+                String icon = parser.getAttributeValue(null, "img");
+                if (icon == null) {
+                    if (parser.getAttributeCount() > 0) {
+                        icon = parser.getAttributeValue(0);
+                    }
+                }
+                iconPackResources.put(parser.getName().toLowerCase(), icon);
+                continue;
+            }
+
+            if (name.equalsIgnoreCase(ICON_SCALE_TAG)) {
+                String factor = parser.getAttributeValue(null, "factor");
+                if (factor == null) {
+                    if (parser.getAttributeCount() > 0) {
+                        factor = parser.getAttributeValue(0);
+                    }
+                }
+                iconPackResources.put(parser.getName().toLowerCase(), factor);
+                continue;
             }
         }
         return entries;
