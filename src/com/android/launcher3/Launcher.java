38de14e9acc638af3f7c5174464157dc5faf810c
maxwen <max.weninger@gmail.com>
2017-11-29 03:21:21 +0100

    Launcher3: add show date only for top widget
    or if no events are upcoming show date centered and bigger
    also calways center the calendar content view
    
    Change-Id: Icca100b875700f69c9492108f3386b14388d7ec5

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -4212,6 +4212,7 @@ public class Launcher extends BaseActivity
             if (Utilities.SHOW_ALL_DAY_EVENTS_PREFERENCE_KEY.equals(key) ||
                     Utilities.SHOW_EVENTS_PERIOD_PREFERENCE_KEY.equals(key) ||
                     Utilities.SHOW_TODAY_PREFERENCE_KEY.equals(key) ||
+                    Utilities.SHOW_EVENTS_PREFERENCE_KEY.equals(key) ||
                     Utilities.WEATHER_ICON_PACK_PREFERENCE_KEY.equals(key)) {
                 if (mTopContainer != null) {
                     mTopContainer.updateSettings();
