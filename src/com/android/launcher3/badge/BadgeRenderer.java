fd3e8508bd4df2d18d266841740a50c59f07b00a
maxwen <max.weninger@gmail.com>
2018-01-17 17:54:30 +0100

    Launcher3: disable DOTS_ONLY for badge renderer
    and show also the number of notifications
    
    Change-Id: Ic2e9df346ad4672c3ad5b288f3d5de5f12cca1fe

--- src/com/android/launcher3/badge/BadgeRenderer.java
+++ src/com/android/launcher3/badge/BadgeRenderer.java
@@ -38,7 +38,7 @@ import com.android.launcher3.graphics.ShadowGenerator;
  */
 public class BadgeRenderer {
 
-    private static final boolean DOTS_ONLY = true;
+    private static final boolean DOTS_ONLY = false;
 
     // The badge sizes are defined as percentages of the app icon size.
     private static final float SIZE_PERCENTAGE = 0.38f;
