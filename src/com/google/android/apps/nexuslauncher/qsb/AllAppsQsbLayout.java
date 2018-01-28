426f5c2f98d38992f08931c8acead8834cd8b47e
ezio84 <brabus84@gmail.com>
2018-01-28 14:34:07 +0100

    Use dark search bar in app drawer for dark theme

--- src/com/google/android/apps/nexuslauncher/qsb/AllAppsQsbLayout.java
+++ src/com/google/android/apps/nexuslauncher/qsb/AllAppsQsbLayout.java
@@ -163,7 +163,7 @@ public class AllAppsQsbLayout extends AbstractQsbLayout implements SearchUiManag
     }
 
     public void onExtractedColorsChanged(final WallpaperColorInfo wallpaperColorInfo) {
-        int color = Themes.getAttrBoolean(mActivity, R.attr.isMainColorDark) ? 0xEBFFFFFE : 0xCCFFFFFE;
+        int color = Themes.getAttrBoolean(mActivity, R.attr.isMainColorDark) ? 0xD9444444 : 0xCCFFFFFE;
         bz(ColorUtils.compositeColors(ColorUtils.compositeColors(color, Themes.getAttrColor(mActivity, R.attr.allAppsScrimColor)), wallpaperColorInfo.getMainColor()));
     }
 
