45621c851cb835e87606573fdc3a86f907110aca
Artem Borisov <dedsa2002@gmail.com>
2018-09-17 18:43:54 +0300

    Use new widget style for Google searchbar
    Change-Id: I7ff5bc2fa8ad04c2fadad3b2e0c6c45b3270d6c0

--- src/com/android/launcher3/qsb/QsbContainerView.java
+++ src/com/android/launcher3/qsb/QsbContainerView.java
@@ -117,6 +117,16 @@ public class QsbContainerView extends FrameLayout {
                 return QsbWidgetHostView.getDefaultView(container);
             }
 
+            if (mWidgetInfo.provider.getClassName().equals("com.google.android.googlequicksearchbox.SearchWidgetProvider")) {
+                try {
+                    int resId = activity.getPackageManager().getReceiverInfo(mWidgetInfo.provider, 128).metaData.getInt("com.google.android.gsa.searchwidget.alt_initial_layout_cqsb", -1);
+                    if (resId != -1) {
+                        mWidgetInfo.initialLayout = resId;
+                    }
+                } catch (Exception e) {
+                }
+            }
+
             AppWidgetManager widgetManager = AppWidgetManager.getInstance(activity);
             InvariantDeviceProfile idp = LauncherAppState.getIDP(activity);
 
@@ -126,6 +136,8 @@ public class QsbContainerView extends FrameLayout {
             opts.putInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT, size.top);
             opts.putInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH, size.right);
             opts.putInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT, size.bottom);
+            opts.putString("attached-launcher-identifier", getActivity().getPackageName());
+            opts.putString("requested-widget-style", "cqsb");
 
             int widgetId = Utilities.getPrefs(activity).getInt(QSB_WIDGET_ID, -1);
             AppWidgetProviderInfo widgetInfo = widgetManager.getAppWidgetInfo(widgetId);
