00e9dfc750007903a28a9b7a45d3ce0c331a2565
maxwen <max.weninger@gmail.com>
2018-11-05 12:40:24 +0100

    Launcher3: add switch to hide search widget
    BRUTE FORCE - here I come
    
    Change-Id: Iaccde127196d71c932e647445ac6ca4abfff967e
    
    Launcher: add resource qsb_show_default to set default for pref_qsb_show
    
    Change-Id: Ifbad5d677039d8cd068edf07c891c1e859c22631

--- src/com/android/launcher3/model/LoaderCursor.java
+++ src/com/android/launcher3/model/LoaderCursor.java
@@ -461,7 +461,7 @@ public class LoaderCursor extends CursorWrapper {
                 // Mark the first row as occupied (if the feature is enabled)
                 // in order to account for the QSB.
                 int spanY = FeatureFlags.EXPANDED_SMARTSPACE.get() ? 2 : 1;
-                screen.markCells(0, 0, countX + 1, spanY, FeatureFlags.QSB_ON_FIRST_SCREEN);
+                screen.markCells(0, 0, countX + 1, spanY,  Utilities.showQsbWidget(mContext));
             }
             occupied.put(item.screenId, screen);
         }
