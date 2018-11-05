f5ade84bdcc752604235a2fe33f1fc070bb4f027
maxwen <max.weninger@gmail.com>
2018-11-05 12:40:24 +0100

    Launcher3: add switch to hide search widget
    BRUTE FORCE - here I come
    
    Change-Id: Iaccde127196d71c932e647445ac6ca4abfff967e
    
    Launcher: add resource qsb_show_default to set default for pref_qsb_show
    
    Change-Id: Ifbad5d677039d8cd068edf07c891c1e859c22631

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -24,6 +24,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.XmlResourceParser;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Xml;
@@ -227,6 +228,14 @@ public class SettingsActivity extends FragmentActivity
             if (getActivity() != null && !TextUtils.isEmpty(getPreferenceScreen().getTitle())) {
                 getActivity().setTitle(getPreferenceScreen().getTitle());
             }
+
+            Preference showQsbWidget = findPreference(Utilities.QSB_SHOW);
+            showQsbWidget.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    new Handler().postDelayed(() -> Utilities.restart(getActivity()), Utilities.WAIT_BEFORE_RESTART);
+                    return true;
+                }
+            });
 
             final DropDownPreference grid = (DropDownPreference) findPreference(GRID_SIZE_PREFERENCE_KEY);
             InvariantDeviceProfile idp = InvariantDeviceProfile.INSTANCE.get(getContext());
