LawnchairLauncher/lawnchair

Only call setCurrentPage in applyLoadPlan if mCurrentPage is outdated
- Apply the same for all 3 setCurrentPage cases as they can all causes page jumping

Bug: 197493120
Test: manual
Change-Id: I5f7013ce3ce4d6fe84c67123618c3bebeeffc43a

quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -1357,21 +1357,22 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
             }
         }
 
+        int targetPage = -1;
         if (mNextPage == INVALID_PAGE) {
             // Set the current page to the running task, but not if settling on new task.
             if (runningTaskId != -1) {
-                setCurrentPage(indexOfChild(newRunningTaskView));
+                targetPage = indexOfChild(newRunningTaskView);
             } else if (getTaskViewCount() > 0) {
-                setCurrentPage(indexOfChild(getTaskViewAt(0)));
+                targetPage = indexOfChild(getTaskViewAt(0));
             }
         } else if (currentTaskId != -1) {
             currentTaskView = getTaskViewByTaskId(currentTaskId);
             if (currentTaskView != null) {
-                int currentTaskViewIndex = indexOfChild(currentTaskView);
-                if (mCurrentPage != currentTaskViewIndex) {
-                    setCurrentPage(currentTaskViewIndex);
-                }
+                targetPage = indexOfChild(currentTaskView);
             }
+        }
+        if (targetPage != -1 && mCurrentPage != targetPage) {
+            setCurrentPage(targetPage);
         }
 
         if (mIgnoreResetTaskId != -1 &&

omnirom/android_packages_apps_Launcher3

Merged-In: I5f7013ce3ce4d6fe84c67123618c3bebeeffc43a
(cherry picked from commit dacb37c1435e68be5c96d374ba18cd58c37a19dd)

quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -1146,19 +1146,23 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         }
         updateTaskSize();
 
+        int targetPage = -1;
         if (mNextPage == INVALID_PAGE) {
             // Set the current page to the running task, but not if settling on new task.
             TaskView runningTaskView = getRunningTaskView();
             if (runningTaskView != null) {
-                setCurrentPage(indexOfChild(runningTaskView));
+                targetPage = indexOfChild(runningTaskView);
             } else if (getTaskViewCount() > 0) {
-                setCurrentPage(indexOfChild(getTaskViewAt(0)));
+                targetPage = indexOfChild(getTaskViewAt(0));
             }
         } else if (currentTaskId != -1) {
             currentTaskView = getTaskView(currentTaskId);
             if (currentTaskView != null) {
-                setCurrentPage(indexOfChild(currentTaskView));
+                targetPage = indexOfChild(currentTaskView);
             }
+        }
+        if (targetPage != -1 && mCurrentPage != targetPage) {
+            setCurrentPage(targetPage);
         }
 
         if (mIgnoreResetTaskId != -1 && getTaskView(mIgnoreResetTaskId) != ignoreResetTaskView) {