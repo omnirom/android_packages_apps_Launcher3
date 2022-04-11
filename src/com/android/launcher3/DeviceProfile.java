baabbce67a48924fa61ea950d0656c412da79625
Danny Lin <danny@kdrag0n.dev>
2022-04-10 18:38:45 -0700

    Launcher3: Add support for toggling taskbar
    - Use new API to enable/disable SysUI taskbar integration
    - User-facing setting to control taskbar eligibility
    - Reload by restarting (live reload would be much more complicated)
    - Update SysUI integration state before reloading
    
    Depends on frameworks/base commit:
        SystemUI: Add API for runtime taskbar config
    
    maxwen: adapted to omni settings
    
    Change-Id: Id1889ca9b102f94cc0a1161f49de88428dcc0943

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -42,6 +42,7 @@ import android.content.res.TypedArray;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.SparseArray;
 import android.view.Surface;
@@ -71,6 +72,8 @@ import com.android.launcher3.util.IconSizeSteps;
 import com.android.launcher3.util.ResourceHelper;
 import com.android.launcher3.util.WindowBounds;
 import com.android.launcher3.util.window.WindowManagerProxy;
+
+import org.omnirom.omnilib.utils.OmniSettings;
 
 import java.io.PrintWriter;
 import java.util.Locale;
@@ -353,7 +356,10 @@ public class DeviceProfile {
         isTablet = info.isTablet(windowBounds);
         isPhone = !isTablet;
         isTwoPanels = isTablet && isMultiDisplay;
-        isTaskbarPresent = (isTablet || (enableTinyTaskbar() && isGestureMode))
+        boolean isTaskBarEnabled = Settings.System.getInt(context.getContentResolver(),
+                OmniSettings.OMNI_ENABLE_TASKBAR,
+                (isTablet || (enableTinyTaskbar() && isGestureMode)) ? 1 : 0) == 1;
+        isTaskbarPresent = isTaskBarEnabled
                 && WindowManagerProxy.INSTANCE.get(context).isTaskbarDrawnInProcess();
 
         // Some more constants.
