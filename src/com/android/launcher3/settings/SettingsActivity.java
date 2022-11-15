70e9d1dfe70f44b23822cf7850668ea7cf031629
maxwen <max.weninger@gmail.com>
2022-11-15 22:10:05 +0100

    Launcher3: add support for hotseat qsb
    Change-Id: I571dfacf67a63872a8f7351ad9c6106397f79505

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -38,7 +38,7 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
-import androidx.preference.DropDownPreference;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceFragmentCompat.OnPreferenceStartFragmentCallback;
@@ -86,6 +86,7 @@ public class SettingsActivity extends FragmentActivity
 
     private static final String NOTIFICATION_DOTS_PREFERENCE_KEY = "pref_icon_badging";
     private static final String GRID_SIZE_PREFERENCE_KEY = "pref_grid";
+    public static final String QSB_LOCATION_PREFERENCE_KEY = "pref_qsb_location";
 
     public static final String EXTRA_FRAGMENT_ARG_KEY = ":settings:fragment_args_key";
     public static final String EXTRA_SHOW_FRAGMENT_ARGS = ":settings:show_fragment_args";
@@ -263,13 +264,13 @@ public class SettingsActivity extends FragmentActivity
                 }
             });
 
-            final DropDownPreference grid = (DropDownPreference) findPreference(GRID_SIZE_PREFERENCE_KEY);
+            final ListPreference grid = (ListPreference) findPreference(GRID_SIZE_PREFERENCE_KEY);
             InvariantDeviceProfile idp = InvariantDeviceProfile.INSTANCE.get(getContext());
             ArrayList<String> entries = new ArrayList<>();
             ArrayList<String> values = new ArrayList<>();
             for (GridOption gridOption : parseAllGridOptions(idp.deviceType)) {
                 values.add(gridOption.name);
-                entries.add("    " + gridOption.numColumns + " x " + gridOption.numRows + "    ");
+                entries.add(gridOption.numColumns + " x " + gridOption.numRows);
             }
 
             grid.setEntries(entries.toArray(new String[entries.size()]));
@@ -310,6 +311,16 @@ public class SettingsActivity extends FragmentActivity
             if (transparentTaskbar != null && !deviceProfile.isTaskbarPresent) {
                 getPreferenceScreen().removePreference(transparentTaskbar);
             }
+
+            final ListPreference qsbLocation = (ListPreference) findPreference(QSB_LOCATION_PREFERENCE_KEY);
+            valueIndex = qsbLocation.findIndexOfValue(qsbLocation.getValue());
+            qsbLocation.setSummary(qsbLocation.getEntries()[valueIndex]);
+            qsbLocation.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    new Handler().postDelayed(() -> Utilities.restart(getActivity()), Utilities.WAIT_BEFORE_RESTART);
+                    return true;
+                }
+            });
         }
 
         @Override
