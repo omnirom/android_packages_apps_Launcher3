f069c2b3900198c688893834dbc49bd4ce06167a
maxwen <max.weninger@gmail.com>
2022-11-15 22:10:05 +0100

    Launcher3: add support for hotseat qsb
    and fix styles to support ListPreference
    
    Change-Id: I571dfacf67a63872a8f7351ad9c6106397f79505
    
    Launcher3: fix crash on taskbar usage
    
    we use search_container_hotseat.xml for phone when using
    qsb in hotseat. So add a new empty one for taskbar
    
    Change-Id: I3df2c9ad432c51e06102b65282bcbc9c4848e6fa
    
    Launcher3: fixup hardocded usages of QSB_ON_FIRST_SCREEN
    
    Change-Id: Id8f6ca9c332f0c53bdf96c944f8daa361adeab1c
    
    Launcher3: fix hotseat qsb widget width
    
    we are not going beyond isScalableGrid in
    recalculateHotseatWidthAndBorderSpace so hotseatQsbWidth is 0
    in that case. To prevent any unwanted side effects add
    a separate API for the width to use if not isQsbInline
    
    Change-Id: I2d49127aa6ea3e10eab043f3f9a102154f1d8458

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -71,6 +71,7 @@ import com.android.launcher3.util.IconSizeSteps;
 import com.android.launcher3.util.ResourceHelper;
 import com.android.launcher3.util.WindowBounds;
 import com.android.launcher3.util.window.WindowManagerProxy;
+import com.android.launcher3.Utilities;
 
 import java.io.PrintWriter;
 import java.util.Locale;
@@ -540,7 +541,7 @@ public class DeviceProfile {
 
         workspaceCellPaddingXPx = res.getDimensionPixelSize(R.dimen.dynamic_grid_cell_padding_x);
 
-        hotseatQsbHeight = res.getDimensionPixelSize(R.dimen.qsb_widget_height);
+        hotseatQsbHeight = Utilities.showHotseatQsbWidget(context) ? res.getDimensionPixelSize(R.dimen.qsb_widget_height) : 0;
         hotseatQsbShadowHeight = res.getDimensionPixelSize(R.dimen.qsb_shadow_height);
         hotseatQsbVisualHeight = hotseatQsbHeight - 2 * hotseatQsbShadowHeight;
 
@@ -884,6 +885,13 @@ public class DeviceProfile {
                     + hotseatQsbVisualHeight
                     + hotseatBarBottomSpacePx;
         }
+    }
+
+    /**
+     * used in Hotseat only for !isQsbInline
+     */
+    public int getHostseatQsbWidth() {
+        return calculateQsbWidth(0);
     }
 
     /**
