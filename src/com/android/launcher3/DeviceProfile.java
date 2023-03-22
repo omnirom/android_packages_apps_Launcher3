fd02918a8b5b09512ca8e4eb77aa748870ace4aa
micky387 <mickaelsaibi@free.fr>
2023-03-22 13:38:36 +0100

    Revert "Launcher3: repect hw button in hotseat padding 2.0"
    This reverts commit b40bf6a7f5326b0fd373867bdb8bf7d380154eb9.

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -172,8 +172,6 @@ public class DeviceProfile {
     public int hotseatBarSizePx;
     public int hotseatBarBottomSpacePx;
     public int hotseatBarEndOffset;
-    public int hotseatBarStartOffset;
-    private boolean mHwButtons;
     public int hotseatQsbSpace;
     public int springLoadedHotseatBarTopMarginPx;
     // Start is the side next to the nav bar, end is the side next to the workspace
@@ -445,10 +443,6 @@ public class DeviceProfile {
         } else {
             hotseatBarEndOffset = 0;
         }
-        mHwButtons = res.getBoolean(R.bool.taskbar_add_hardware_buttons);
-        hotseatBarStartOffset = mHwButtons ? 3 * res.getDimensionPixelSize(R.dimen.taskbar_nav_buttons_size)
-                + 2 * res.getDimensionPixelSize(R.dimen.taskbar_contextual_button_padding)
-                + res.getDimensionPixelSize(R.dimen.taskbar_hotseat_nav_spacing) : 0;
 
         overviewTaskMarginPx = res.getDimensionPixelSize(R.dimen.overview_task_margin);
         overviewTaskIconSizePx = res.getDimensionPixelSize(R.dimen.task_thumbnail_icon_size);
@@ -1179,7 +1173,7 @@ public class DeviceProfile {
 
             // Push icons to the side
             int requiredWidth = getHotseatRequiredWidth();
-            int hotseatWidth = Math.min(requiredWidth, availableWidthPx - hotseatBarEndOffset - hotseatBarStartOffset);
+            int hotseatWidth = Math.min(requiredWidth, availableWidthPx - hotseatBarEndOffset);
             int sideSpacing = (availableWidthPx - hotseatWidth) / 2;
 
             hotseatBarPadding.set(sideSpacing, hotseatBarTopPadding, sideSpacing,
