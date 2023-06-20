8907632060f6c4ed95e33eb59c97b88b4bd2a6fd
micky387 <mickaelsaibi@free.fr>
2023-06-20 10:36:03 +0200

    Revert "Launcher3: center hotseat in taskbar also with inline nav buttons"
    This reverts commit 466c214bf9b94efb6b2c0b63ebd17c2783ce52df.
    
    Change-Id: I71a69fe4555f58c3559574c8a4691ada15041786

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -1291,12 +1291,9 @@ public class DeviceProfile {
             int leftSpacing = (availableWidthPx - hotseatWidth - hotseatBarStartOffset) / 2;
             int rightSpacing = leftSpacing;
             // Hotseat aligns to the left with nav buttons
-            // omni: no thats looks aweful - just center it
             if (hotseatBarEndOffset > 0) {
-                //leftSpacing = Math.max(hotseatBarStartOffset, hotseatBarEndOffset);
-                //rightSpacing = availableWidthPx - hotseatWidth - leftSpacing + hotseatBorderSpace;
-                leftSpacing = (availableWidthPx - hotseatWidth - Math.max(hotseatBarStartOffset, hotseatBarEndOffset)) / 2;
-                rightSpacing = leftSpacing;
+                leftSpacing = Math.max(hotseatBarStartOffset, inlineNavButtonsEndSpacing);
+                rightSpacing = availableWidthPx - hotseatWidth - leftSpacing + hotseatBorderSpace;
             }
 
             hotseatBarPadding.set(leftSpacing, hotseatBarTopPadding, rightSpacing,
