63e1a1e249ee7c8fb6821da887681fa23b9609da
Michael Bestas <mkbestas@lineageos.org>
2024-03-10 18:37:23 +0200

    fixup! QuickStep: Google Feed integration
    Change-Id: I517c94477565fcbfd189972caadc4f1bfa433895

--- src_overlay/mock/com/android/launcher3/overlay/OverlayCallbackImpl.java
+++ src_overlay/mock/com/android/launcher3/overlay/OverlayCallbackImpl.java
@@ -72,36 +72,23 @@ public class OverlayCallbackImpl
     }
 
     @Override
-    public boolean startSearch(byte[] config, Bundle extras) {
-        return false;
+    public void onActivityStarted() {
     }
 
     @Override
-    public void onActivityCreated(Activity activity, Bundle bundle) {
-        // Not called
+    public void onActivityResumed() {
     }
 
     @Override
-    public void onActivityStarted(Activity activity) {
+    public void onActivityPaused() {
     }
 
     @Override
-    public void onActivityResumed(Activity activity) {
+    public void onActivityStopped() {
     }
 
     @Override
-    public void onActivityPaused(Activity activity) {
-    }
-
-    @Override
-    public void onActivityStopped(Activity activity) {
-    }
-
-    @Override
-    public void onActivitySaveInstanceState(Activity activity, Bundle bundle) { }
-
-    @Override
-    public void onActivityDestroyed(Activity activity) {
+    public void onActivityDestroyed() {
     }
 
     @Override
