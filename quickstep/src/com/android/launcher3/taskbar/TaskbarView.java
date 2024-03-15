d60b6e21a8267043030709e41a3b9a0610e61089
micky387 <mickaelsaibi@free.fr>
2024-03-15 19:13:13 -0400

    Revert "Revert "Launcher3: add support for hotseat qsb""
    This reverts commit 0e45e36f35d92122cde07eb231ac987401c41e80.
    
    Change-Id: I356e9b4954da3df04b47edc247a52696b16c750e

--- quickstep/src/com/android/launcher3/taskbar/TaskbarView.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarView.java
@@ -174,7 +174,7 @@ public class TaskbarView extends FrameLayout implements FolderIcon.FolderIconPar
         }
 
         // TODO: Disable touch events on QSB otherwise it can crash.
-        mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat, this, false);
+        mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat_taskbar, this, false);
     }
 
     @DrawableRes
